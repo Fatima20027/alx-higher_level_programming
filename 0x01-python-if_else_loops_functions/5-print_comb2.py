7ff90a46ec1d2c8ece348c5092380f0d0a26d1de
Fatima20027 <fatima.hailou@icloud.com>
2024-03-12 23:42:18 +0000

    task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number != 99:
+        end=", "
+    else:
+        end="\n"
+    if number < 10:
+        print("0{number}".format(number=number), end=end)
+    else:
+        print("{number}".format(number=number), end=end)
