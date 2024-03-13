14884b1df14c1f3d77325bfd684c54cb3b310de7
Fatima20027 <fatima.hailou@icloud.com>
2024-03-13 00:21:51 +0000

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for j in range(0, 9):
+for o in range(0, 9):
     for i in range(1, 10):
-        if j == 8 and i == 9:
+        if o == 8 and i == 9:
             end = "\n"
         else:
             end = ", "
-        if j < i:
-            print("{j}{i}".format(j=j, i=i), end=end)
+        if o < i:
+            print("{o}{i}".format(o=o, i=i), end=end)
