90976d5783aa3c9fc79f2f52ff4cdd069b93c96a
Fatima20027 <fatima.hailou@icloud.com>
2024-03-18 15:21:21 +0000

    changes in task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        length = len(sentence)
-        tuple = ()
-        if length == 0:
-                tuple = 0, None
-        else:
-                tuple = length, sentence[0]
-        return tuple 
+    length = len(sentence)
+    tuple = ()
+    if length == 0:
+        tuple = 0, None
+    else:
+        tuple = length, sentence[0]
+    return tuple
