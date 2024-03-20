f43b026b5f0ec516418ccafe0773eaf9e91ab942
Fatima20027 <fatima.hailou@icloud.com>
2024-03-20 18:11:40 +0000

    secod try

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 def uniq_add(my_list=[]):
-    uniq_add = set(my_list)
+    new_list = set(my_list)
     result = 0
-    for i in uniq_add:
+    for i in new_list:
         result += i
     return result
