d3b43b5fb9f9a4789bf12ef6e3fcaa6a4dd64c7a
Fatima20027 <fatima.hailou@icloud.com>
2024-04-02 23:12:13 +0000

    another attempt task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,6 +7,7 @@ class Square:
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
-    
+
     def __init__(self, size):
+        """Initializes the data."""
         self.__size = size
