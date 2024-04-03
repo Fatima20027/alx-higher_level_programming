875b5974167dae8cffa402ad2b91bdf0fac92250
Fatima20027 <fatima.hailou@icloud.com>
2024-04-03 00:50:32 +0000

    another attempt

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define a class called Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initializes the data."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
