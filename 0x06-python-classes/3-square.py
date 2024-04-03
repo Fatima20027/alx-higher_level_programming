61f3982fc16b9738e111ffa89ecba9461f7911b3
Fatima20027 <fatima.hailou@icloud.com>
2024-04-03 00:21:47 +0000

    first attempt

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a class called Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size=0):
+        """Initializes the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
