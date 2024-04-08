1ce327fe2e87fa20b0a3151f5dbafd90d1ac8a1a
Fatima20027 <fatima.hailou@icloud.com>
2024-04-08 23:47:59 +0000

    4th attempt

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -68,3 +68,6 @@ class Rectangle:
         """Returns a string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
+        
+        return ("\n".join(["".join(["#" for i in range(self.__width)])
+                for j in range(self.__height)]))
