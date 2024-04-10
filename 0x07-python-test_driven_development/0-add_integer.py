c80de0855a504bf3ebb4d15467d693a4adae6471
Fatima20027 <fatima.hailou@icloud.com>
2024-04-10 01:04:22 +0000

    pushing everything

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if not isinstance(a,(int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     if isinstance(a, float):
         a = int(a)
 
@@ -12,6 +12,7 @@ def add_integer(a, b=98):
         b = int(b)
 
     return a + b
+
 
 if __name__ == "__main__":
     import doctest
