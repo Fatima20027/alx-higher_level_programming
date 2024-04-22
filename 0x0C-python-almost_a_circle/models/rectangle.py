63f3bdc57781b2683c1bd00f8b3382a0cf3d2be1
Fatima20027 <fatima.hailou@icloud.com>
2024-04-22 16:07:15 +0100

    wrote the TDD

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,19 @@
+#!/usr/bin/python3
+'''
+    Class Rectangle
+'''
+
+
 from models.base import Base
+
+
 class Rectangle(Base):
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
@@ -9,26 +23,34 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''
+            Returning private attribute
+        '''
         return self.__width
-    
-    
+
     @width.setter
     def width(self, value):
+        '''
+            Setting private attribute
+        '''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
-    
-
         self.__width = value
 
-    
     @property
     def height(self):
+        '''
+            Returning private attribute
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''
+            Setting private attribute
+        '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -37,24 +59,34 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        
+        '''
+            Returning private attribute
+        '''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''
+            Setting private attribute
+        '''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
-
     @property
     def y(self):
+        '''
+            Returning private attribute
+        '''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''
+            Setting private attribute
+        '''
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -62,9 +94,15 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        '''
+            Returns the area of the rectangle
+        '''
         return self.__width * self.__height
 
     def display(self):
+        '''
+            Prints to stdout the representation of the rectangle
+        '''
         for row in range(self.__y):
             print("")
         for row in range(self.__height):
@@ -72,8 +110,6 @@ class Rectangle(Base):
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
-
-
 
     def update(self, *args, **kwargs):
         '''
@@ -87,6 +123,12 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 self.__setattr__(key, value)
 
-    
-
-        
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'id': getattr(self, "id"),
+                'width': getattr(self, "width"),
+                'height': getattr(self, "height"),
+                'x': getattr(self, "x"),
+                'y': getattr(self, "y"),}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,10 +3,58 @@
 """
 from models.rectangle import Rectangle
 class Square(Rectangle):
+    """Module Representation of Square"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization a Square
         """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+    @property
+    def size(self):
+        """module Square size getter
+        """
+        return self.width
+    
+    
+    @size.setter
+    def size(self, value):
+        """module Square size setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+    
+
+        self.width = value
+        self.height = value
+
+    
+    def update(self, *args, **kwargs):
+        """module update square
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.size = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+        
+        else:
+            for value, key in kwargs.items():
+                setattr(self, value, key)
+
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'size': getattr(self, "size"),
+                'id': getattr(self, "id"),}
