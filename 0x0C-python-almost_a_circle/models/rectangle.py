7daced5c975af2b0881a768fe4f9350686a13d2d
Fatima20027 <fatima.hailou@icloud.com>
2024-04-22 19:44:00 +0100

    models fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,10 +33,7 @@ class Rectangle(Base):
         '''
             Setting private attribute
         '''
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
+        self.setter_validation("width", value)
         self.__width = value
 
     @property
@@ -51,10 +48,7 @@ class Rectangle(Base):
         '''
             Setting private attribute
         '''
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
+        self.setter_validation("height", value)
         self.__height = value
 
     @property
@@ -69,10 +63,7 @@ class Rectangle(Base):
         '''
             Setting private attribute
         '''
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
+        self.setter_validation("x", value)
         self.__x = value
 
     @property
@@ -87,10 +78,7 @@ class Rectangle(Base):
         '''
             Setting private attribute
         '''
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
+        self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
