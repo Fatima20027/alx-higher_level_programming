2aa577c30073d10ccf8fe8bbb08f0505cdc847df
Fatima20027 <fatima.hailou@icloud.com>
2024-05-21 17:59:38 +0100

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 --  create a table called first_table in the current database in my MySQL server.
-CREATE TABLE first_table (
+CREATE TABLE IF NOT first_table (
 	id INT,
-	name VARCHAR(256),
+	name VARCHAR(256)
 );
