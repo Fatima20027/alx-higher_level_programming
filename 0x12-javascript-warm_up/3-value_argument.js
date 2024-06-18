cbc055b8126616712a842c8a7c61013dccc1c2bf
Fatima20027 <fatima.hailou@icloud.com>
2024-06-18 23:15:15 +0100

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
-  console.log('No argument');
+if (process.argv.length > 2) {
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
 } else {
-  console.log(process.argv[2]);
+  console.log('No argument');
 }
