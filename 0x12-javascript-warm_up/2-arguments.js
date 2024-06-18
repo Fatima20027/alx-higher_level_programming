e4ad2c21d45d29fe81ba8000ef254eac9fef4de3
Fatima20027 <fatima.hailou@icloud.com>
2024-06-18 19:40:50 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-function argument () {
-  if (process.argv.length <= 2) {
-    console.log('No argument');
-  } else {
-    console.log('Argument found');
-  }
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }
-
-argument();
