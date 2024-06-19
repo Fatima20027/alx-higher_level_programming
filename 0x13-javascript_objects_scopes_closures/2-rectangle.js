2d1e68cb306441b4b604e80feafc956f70c89996
Fatima20027 <fatima.hailou@icloud.com>
2024-06-19 18:34:59 +0100

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  // class Rectangle that defines a rectangle
+  constructor (w, h) {
+    // creates an empty object if w or h is equal to 0 or not a positive integer
+    if (!(w > 0 && h > 0)) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
