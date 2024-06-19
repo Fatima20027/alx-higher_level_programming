33e437e3695a8cff41e37678de125f5340dd5023
Fatima20027 <fatima.hailou@icloud.com>
2024-06-19 20:45:37 +0100

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  // class Rectangle that defines a rectangle
+  constructor (w, h) {
+    // creates an empty object if w or h is equal to 0 or not a positive integer
+    if ((w > 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
