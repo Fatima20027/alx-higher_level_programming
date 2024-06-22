cc938c77a03b7f4faf3dbbd8135522793f155981
Fatima20027 <fatima.hailou@icloud.com>
2024-06-22 22:32:27 +0100

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,35 +3,26 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL
-    try:
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        # Execute the query
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        # Fetch all rows and print each row tuple
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        sys.exit(1)
 
-    except Exception as e:
-        sys.exit(1)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
 
-    sys.exit(0)
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
