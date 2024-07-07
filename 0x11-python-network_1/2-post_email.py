929f3b2c1d275b2a402d58d84c33ff3cc754f53e
Fatima20027 <fatima.hailou@icloud.com>
2024-07-07 19:15:35 +0100

    task-2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
-2-post_email.py
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response"""
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2].encode('utf-8')
+
+req = urllib.request.Request(url, data=email, method='POST')
+with urllib.request.urlopen(req) as r:
+    response = r.read().decode('utf-8')
+    print("Your email is: {}".format(response))
