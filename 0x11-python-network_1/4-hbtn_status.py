97cad45318c7ea0003e09c6db52788a853555e07
Fatima20027 <fatima.hailou@icloud.com>
2024-07-09 22:15:17 +0100

    taks-4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
-4-hbtn_status.py
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        html = r.read().decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
