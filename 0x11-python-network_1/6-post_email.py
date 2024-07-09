2d3acf354507883183373e037221715f7204b423
Fatima20027 <fatima.hailou@icloud.com>
2024-07-10 00:05:53 +0100

    task-6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
-6-post_email.py
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response."""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    load = {'email': argv[2]}
+    respo = requests.post(argv[1], data=load)
+    print(respo.text)
