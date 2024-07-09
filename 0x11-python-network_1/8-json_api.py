ad1ac03a0bc0e5e8ca5b283d255ebd4239bde31b
Fatima20027 <fatima.hailou@icloud.com>
2024-07-10 00:30:43 +0100

    task-8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
-8-json_api.py
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")
