b230a9f0334c31cb3a62c2b01f6549ca5bb04f06
Fatima20027 <fatima.hailou@icloud.com>
2024-07-09 21:55:13 +0100

    task-3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
-3-error_code.py
+#!/usr/bin/python3
+""" Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8)."""
+
+import urllib.error
+import urllib.parse
+import urllib.request
+import sys
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+
+        with urllib.request.urlopen(url) as r:
+            print(r.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
