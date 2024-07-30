7afcaa618bfabda7e02b663f50d0f3e4d78752ee
Fatima20027 <fatima.hailou@icloud.com>
2024-07-30 20:33:04 +0100

    task2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request')
+const res = request.get(url);
+res.on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
+
