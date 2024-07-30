3c961519595fea80d4b964e856a1a6ea35283121
Fatima20027 <fatima.hailou@icloud.com>
2024-07-30 18:15:18 +0100

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 try {
-    const readMe = fs.readFileSync(filePath, 'utf-8');
-    console.log(readMe);
+  const readMe = fs.readFileSync(filePath, 'utf-8');
+  console.log(readMe);
 } catch (err) {
-    console.error(err); // Print the error object
-    process.exit(1);
+  console.error(err); // Print the error object
+  process.exit(1);
 }
